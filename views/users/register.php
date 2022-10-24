2b35fa3335d2e18fa4641d0ea7383e27e3b0dd52
Krystian-lab <116524932+Krystian-lab@users.noreply.github.com>
2022-10-25 01:16:54 +0200

    Add files via upload

--- views/users/register.php
+++ views/users/register.php
@@ -1,10 +1,10 @@
 
-
+<section class="signin-page account">
 <div class="container">
   <div class="row">
     <div class="col-md-8 mx-auto">
 	<form method="post" name="registerFrom" id="registerFrom" action="<?php echo ROOT_URL; ?>users/createAccount">
-		<tbody>
+		<table calass ="table">
 			<tr>
 				<td class="reg_tab_title" colspan="2">
 				   Dane identyfikacyjne
@@ -23,11 +23,11 @@
 			</tr>
 </br>
 			<tr>
-				<td align="right" width="40%" class="label" id="td_name" style=" visibility: hidden;">
-					<p id="name_firm">Imię i nazwisko</p>
+				<td align="right" width="40%" class="label" id="td_name" >
+					<p id="name_firm">Nazwa Firmy</p>
 				</td>
 				<td>
-				   <input id="name_firm" type="text" name="name_firm" class="name_firm" value="">
+				   <input style="width:230px;" id="in_name_firm" type="text" name="name_firm" class="name_firm" value="">
 			   </td>
 			</tr>
 			</br>
@@ -38,8 +38,8 @@
 					Ulica, nr domu
 				</td>
 				<td>
-				   <input id="street" type="text" name="street" value="" class="in_street ">
-				   <input id="localNo" type="text" name="localNo" maxlength="8" class="in_localNo"    value="">
+				   <input style="width:165px;" id="in_street" type="text" name="street" value="" class="in_street ">
+				   <input style="width:60px;" id="in_localNo" type="text" name="localNo" maxlength="8" class="in_localNo"    value="">
 				</td>
 			</tr>
 			</br>
@@ -49,8 +49,8 @@
 					Miejscowość,Kod pocztowy
 				</td>
 				<td>  
-					<input id="place" type="text" name="place" value="" class="in_place">
-					<input id="postCode" type="text" name="postCode" maxlength="6" class="in_postCode" value="">
+					<input style="width:165px;" id="place" type="text" name="place" value="" class="in_place">
+					<input style="width:60px;" id="in_postCode" type="text" name="postCode" maxlength="6" class="in_postCode" value="">
 			   </td>
 			</tr>
 			</br>
@@ -59,7 +59,7 @@
 					Województwo
 				</td>
 				<td>
-				   <select id="voivodeship" name="voivodeship" class="voivodeship">
+				   <select style="width:230px;" id="voivodeship" name="voivodeship" class="voivodeship">
 					   <option value="0">Województwo</option>
 					</select>
 			   </td>
@@ -70,8 +70,8 @@
 					 Telefon 
 				</td>
 				<td>
-				   <input id="phonePrefix" type="text" name="phonePrefix" value="+48" class="in_phonePrefix ">
-				   <input id="phone" type="text" name="phone" value=""  class="in_phone" >
+				   <input style="width:40px;" id="in_phonePrefix" type="text" name="phonePrefix" value="+48" class="in_phonePrefix ">
+				   <input style="width:185px;" id="phone" type="text" name="phone" value=""  class="in_phone" >
 			   </td>
 			</tr>
 			</br>
@@ -80,21 +80,22 @@
 					E-mail
 				</td>
 				<td>
-				   <input id="Email" type="mail" name="Email" value=""  class="in_Email">
+				   <input style="width:230px;" id="in_Email" type="mail" name="Email" value=""  class="in_Email">
 				</td>
 			</tr>
 		
 			<tr>
 				<td align="right" width="40%" class="label" id="td_PESEL">
-					<p id="PESEL_NIP">PESEL</p>
+					<p id="PESEL_NIP">NIP</p>
 				</td>
 				<td>
-				   <input id="PESEL_NIP" type="text" name="PESEL_NIP" value=""  class="in_PESEL_NIP" ">
+				   <input style="width:230px;" id="in_PESEL_NIP" type="text" name="PESEL_NIP" value=""  class="in_PESEL_NIP" ">
 				</td>
 			</tr>
 			
 			
-		</tbody>
+		</table>
+		<div class=hoverdiv>
     <button type="submit" class="form-submit" id= "form-submit">Zapisz</button>
  </form>
    
