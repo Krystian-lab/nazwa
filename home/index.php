552c13ddb97731132da840ea1e8ecb2464e8b099
Krystian-lab <116524932+Krystian-lab@users.noreply.github.com>
2022-10-25 01:17:14 +0200

    Add files via upload

---
+++ home/index.php
@@ -0,0 +1,17 @@
+<section class="signin-page account">
+<div class="container">
+		<div class="row">
+			
+			<div class="col-lg-12">
+				<div class="block">
+				<h1>Zadanie rekrutacyjne Nazwa.pl</h1>
+				<p class="lead">W zakładce "Zarejestruj klienta" znajduję się formluarz z danymi klienta.  </p>
+					
+				</div>
+			</div>
+		
+		</div>
+	</div>
+
+</section>
+
