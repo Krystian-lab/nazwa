305ee2606f6b184b9da42e1349f1c51668ccc604
Krystian-lab <116524932+Krystian-lab@users.noreply.github.com>
2022-10-25 01:15:55 +0200

    Add files via upload

---
+++ views/client/index.php
@@ -0,0 +1,15 @@
+
+
+<section class="signin-page account">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="block">
+                    <h2 class="text-center">Wprowadź adres /json/:id aby uzyskać dane w formacie json lub html/:id by uzyskać widok w formacie html</h2>
+                
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ views/client/json.php
@@ -0,0 +1,6 @@
+<?php 
+echo json_encode($model, JSON_PRETTY_PRINT);
+
+?>
+<div style="min-height: 775px;">
+</div>
