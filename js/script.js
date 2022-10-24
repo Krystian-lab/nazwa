9b3a9082de39ab2e85d13dfbb1db3fed626453bd
Krystian-lab <116524932+Krystian-lab@users.noreply.github.com>
2022-10-25 01:02:25 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 (function ($) {
   'use strict';
-
-  /* ========================================================================= */
-  /*	Page Preloader
-  /* ========================================================================= */
   $(window).on('load', function () {
     $('#preloader').fadeOut('slow', function () {
       $(this).remove();
@@ -16,7 +12,6 @@
          success:function(data)
          {
           var i = 1,
-         
           select = document.getElementById('voivodeship');
       
           for (var key in data) {
@@ -25,83 +20,104 @@
           opt.innerHTML = data[key];
           select.appendChild(opt);
           i++;
-      }
-             console.log(data);
+          }
          }
       });
     });
   });
 
+  $('input[type=radio][name=firm]').change(function() {
+    if (this.value == '1') {
+      let text = document.getElementById("name_firm").innerHTML; 
+      let result = text.replace(text, "Nazwa Firmy");
+      document.getElementById("name_firm").innerHTML = result;
+      let text2 = document.getElementById("PESEL_NIP").innerHTML; 
+      let result2 = text2.replace(text2, "NIP");
+      document.getElementById("PESEL_NIP").innerHTML = result2;
+    }
+    else if (this.value == '0') {
+      let text = document.getElementById("name_firm").innerHTML; 
+      let result = text.replace(text, "Imię i Nazwisko");
+      document.getElementById("name_firm").innerHTML = result;
+      let text2 = document.getElementById("PESEL_NIP").innerHTML; 
+      let result2 = text2.replace(text2, "PESEL");
+      document.getElementById("PESEL_NIP").innerHTML = result2;
+      let PESEL_NIP = document.getElementById('in_PESEL_NIP').value;
+      console.log(PESEL_NIP);
+    }
+});
  
-  let text = document.getElementById("demo").innerHTML; 
-  let result = text.replace(/blue/gi, "red");
-  document.getElementById("demo").innerHTML = result;
-
+ 
   /* ========================================================================= */
   /*   Contact Form Validating
   /* ========================================================================= */
 
 
-  // $('#form-submit').on('click', function (e) {
-  //   e.preventDefault();
-  //   var error = false;
-  //   var name = $('#name').val();
-  //   var email = $('#email').val();
-  //   var subject = $('#subject').val();
-  //   var message = $('#message').val();
+  $('input').change(function() {
+  
+    console.log('test');
+    var error = false;
+    var name = document.getElementById('in_name_firm').value;
+    var email = document.getElementById('in_Email').value;
+    var street = document.getElementById('in_street').value;
+    var postCode =document.getElementById('in_postCode').value;
+    var localNo = document.getElementById('in_localNo').value;
+    var phonePrefix = document.getElementById('in_phonePrefix').value;
+    let PESEL_NIP = document.getElementById('in_PESEL_NIP').value;
+    console.log(error);
+    if (!PESEL_NIP) {
+      (error = true);
+      console.log(PESEL_NIP);
+      $('#PESEL_NIP').css('border-color', '#D8000C');
+    } else {
+      $('#PESEL_NIP').css('border-color', '#666');
+    }
+    if (!phonePrefix) {
+      (error = true);
+      $('#phonePrefix').css('border-color', '#D8000C');
+    } else {
+      $('#phonePrefix').css('border-color', '#666');
+    }
+    if (!localNo ) {
+      (error = true);
+      $('#localNo').css('border-color', '#D8000C');
+    } else {
+      $('#localNo').css('border-color', '#666');
+    }
+    if (!name ) {
+      (error = true);
+      $('#name_firm').css('border-color', '#D8000C');
+    } else {
+      $('#name_firm').css('border-color', '#666');
+    }
+    if (!email || email.indexOf('@') === '-1') {
+      (error = true);
+      $('#Email').css('border-color', '#D8000C');
+    } else {
+      $('#Email').css('border-color', '#666');
+    }
+    if (!street ) {
+      (error = true);
+      $('#street').css('border-color', '#D8000C');
+    } else {
+      $('#street').css('border-color', '#666');
+    }
+    if (!postCode ) {
+      (error = true);
+      $('#postCode').css('border-color', '#D8000C');
+    } else {
+      $('#postCode').css('border-color', '#666');
+    }
+  
+if(error==true)
+{
+  $(':input[type="submit"]').prop('disabled', true);
+  
+}
+if(error==false)
+{
+  $(':input[type="submit"]').prop('disabled', false );
+}
+  });
 
-  //   if (name.length === 0) {
-  //     (error = true);
-  //     $('#name').css('border-color', '#D8000C');
-  //   } else {
-  //     $('#name').css('border-color', '#666');
-  //   }
-  //   if (email.length === 0 || email.indexOf('@') === '-1') {
-  //     (error = true);
-  //     $('#email').css('border-color', '#D8000C');
-  //   } else {
-  //     $('#email').css('border-color', '#666');
-  //   }
-  //   if (subject.length === 0) {
-  //     (error = true);
-  //     $('#subject').css('border-color', '#D8000C');
-  //   } else {
-  //     $('#subject').css('border-color', '#666');
-  //   }
-  //   if (message.length === 0) {
-  //     (error = true);
-  //     $('#message').css('border-color', '#D8000C');
-  //   } else {
-  //     $('#message').css('border-color', '#666');
-  //   }
-
-  //   //now when the validation is done we check if the error variable is false (no errors)
-  //   if (error === false) {
-  //     //disable the submit button to avoid spamming
-  //     //and change the button text to Sending...
-  //     $('#contact-submit').attr({
-  //       'disabled': 'false',
-  //       'value': 'Sending...'
-  //     });
-
-  //     /* using the jquery's post(ajax) function and a lifesaver
-  //     function serialize() which gets all the data from the form
-  //     we submit it to send_email.php */
-  //     $.post('sendmail.php', $('#contact-form').serialize(), function (result) {
-  //       //and after the ajax request ends we check the text returned
-  //       if (result === 'sent') {
-  //         //if the mail is sent remove the submit paragraph
-  //         $('#cf-submit').remove();
-  //         //and show the mail success div with fadeIn
-  //         $('#mail-success').fadeIn(500);
-  //       } else {
-  //         //show the mail failed div
-  //         $('#mail-fail').fadeIn(500);
-  //         //re enable the submit button by removing attribute disabled and change the text back to Send The Message
-  //         $('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
-  //       }
-  //     });
-  //   }
-  // });
-
-})(jQuery);
+ })(jQuery);
